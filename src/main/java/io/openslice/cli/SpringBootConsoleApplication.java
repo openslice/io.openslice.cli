94ecada6b45d6b3b272cfe7091f67fa2ad404614
Christos Tranoris <tranoris@gmail.com>
2019-12-07 22:58:58 +0200

    adding test

--- src/main/java/io/openslice/cli/SpringBootConsoleApplication.java
+++ src/main/java/io/openslice/cli/SpringBootConsoleApplication.java
@@ -7,12 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import static java.util.Arrays.asList;
 
+import io.openslice.tmf.common.model.service.Note;
+import io.openslice.tmf.common.model.service.ServiceSpecificationRef;
 import io.openslice.tmf.scm633.model.ServiceCatalog;
 import io.openslice.tmf.scm633.model.ServiceSpecification;
+import io.openslice.tmf.so641.model.ServiceOrder;
+import io.openslice.tmf.so641.model.ServiceOrderCreate;
+import io.openslice.tmf.so641.model.ServiceOrderItem;
+import io.openslice.tmf.so641.model.ServiceRestriction;
 
 /**
  * 1. Act as main class for spring boot application 2. Also implements
@@ -33,6 +40,8 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
 		ctx.close();
 	}
 
+	private ServiceSpecification specToOrder;
+
 	/**
 	 * This method will be executed after the application context is loaded and
 	 * right before the Spring Application main method is completed.
@@ -52,7 +61,7 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
 
 		//asList(sc).forEach(System.out::println);
 		asList(sc).forEach((s) -> {
-			System.out.println( s.getName());
+			System.out.printf(  "| %s | %s \n", s.getId(), s.getName() );
 		});
 
 		ResponseEntity<ServiceSpecification[]> response2 = restTemplate.getForEntity(
@@ -61,9 +70,39 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
 
 		ServiceSpecification[] sp = response2.getBody();
 
+		System.out.printf(  "--------------SERVICE SPECS------------------ \n" );
 		asList(sp).forEach((s) -> {
-			System.out.println( s.getName());
+			System.out.printf(  "| %s | %s \n", s.getId(), s.getName() );
+			if ( s.getName().equals("A VINNI Service Example")) {
+				specToOrder = s;
+			}
 		});
 
+		ServiceOrderCreate servOrder = new ServiceOrderCreate();
+		Note noteItem = new Note();
+		noteItem.text("test note");
+		servOrder.addNoteItem(noteItem);
+		
+		ServiceOrderItem soi = new ServiceOrderItem();
+		servOrder.getOrderItem().add(soi);
+		
+		ServiceRestriction serviceRestriction = new ServiceRestriction();
+		ServiceSpecificationRef aServiceSpecificationRef = new ServiceSpecificationRef();
+		aServiceSpecificationRef.setId( specToOrder.getId() );
+		
+		serviceRestriction.setServiceSpecification(aServiceSpecificationRef );
+		soi.setService(serviceRestriction );
+		
+		HttpEntity<ServiceOrderCreate> request = new HttpEntity<>( servOrder );
+		ResponseEntity<ServiceOrder> responseOrder = restTemplate.postForEntity(
+				"http://localhost:13082/tmf-api/serviceOrdering/v4/serviceOrder",
+				request,
+				ServiceOrder.class);
+
+		ServiceOrder sor = responseOrder.getBody();
+
+		System.out.printf(  "--------------SERVICE ORDER------------------ \n" );
+		System.out.printf(  "| %s | %s \n", sor.getId(), sor.getOrderItem().toString() );
+		
 	}
 }
